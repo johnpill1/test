159b60cfca32708a5bf921eb5cac4a0365cdceca
John Pill <john_pill1@hotmail.com>
2022-08-18 20:59:36 +0100

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by John Pill on 18/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by John Pill on 18/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
